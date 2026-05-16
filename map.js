cc6fd5d2245905e828107ba3eec8a1e153590df4
DiegoIno1 <dinostroza@ucsd.edu>
2026-05-15 19:54:26 -0700

    step 2

--- map.js
+++ map.js
@@ -1,13 +1,8 @@
 // Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
-// Check that Mapbox GL JS is loaded
-console.log('Mapbox GL JS Loaded:', mapboxgl);
-
-// Set your Mapbox access token here
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGllZ29pbm8xIiwiYSI6ImNtcDdqejZ4YzAzeXozM3B4ZjFoeDNldmoifQ.IVfoP8b-9V1PsWSfhO53kQ';
 
-// Initialize the map
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/diegoino1/cmp7qseda000x01sm3kcf5wc9',
@@ -15,4 +10,37 @@ const map = new mapboxgl.Map({
   zoom: 12,
   minZoom: 5,
   maxZoom: 18,
+});
+
+// Shared style for both bike lane layers
+const bikeLanePaint = {
+  'line-color': '#32D400',
+  'line-width': 3,
+  'line-opacity': 0.6,
+};
+
+map.on('load', async () => {
+  // Boston bike lanes
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+  });
+  map.addLayer({
+    id: 'boston-bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: bikeLanePaint,
+  });
+
+  // Cambridge bike lanes
+  map.addSource('cambridge_route', {
+    type: 'geojson',
+    data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
+  });
+  map.addLayer({
+    id: 'cambridge-bike-lanes',
+    type: 'line',
+    source: 'cambridge_route',
+    paint: bikeLanePaint,
+  });
 });
